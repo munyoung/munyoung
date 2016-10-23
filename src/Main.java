0d8573815d8b86f9d74f04b7810969e531b000d2
young <young@LG>
2016-10-23 16:59:48 +0900

    完璧ソース

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,84 @@
 import java.util.Scanner;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Coffee CoffeeList[] = new Coffee[10];
-		
-		System.out.println("追加するメニュー情報を入力してください。");
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("コーヒー名：");
-		String name = sc.next();
-		System.out.println("金額：");
-		int price = sc.nextInt();
 
-		sc.close();
-		
+		Scanner sc = new Scanner(System.in);
 		MenuControl menuControl = new MenuControl();
-		menuControl.insertCoffee(CoffeeList, name, price);
+		Coffee CoffeeList[] = new Coffee[10];
+
+		while (true) {
+
+			int menu = 0;
+			System.out.println("*****メニュー*****");
+			System.out.println("1. メニュー閲覧");
+			System.out.println("2. メニュー追加");
+			System.out.println("3. メニュー削除");
+			System.out.println("4. 金額修正");
+			System.out.println("9. 終了");
+
+			menu = sc.nextInt();
+
+			switch (menu) {
+
+			case 1: {
+
+				menuControl.showCoffeeInform(CoffeeList);
+
+				break;
+			}
+
+			case 2: {
+
+				System.out.println("追加するメニュー情報を入力してください。");
+
+				System.out.println("コーヒー名：");
+				String name = sc.next();
+				System.out.println("金額：");
+				int price = sc.nextInt();
+
+				menuControl.insertCoffee(CoffeeList, name, price);
+
+				break;
+			}
+
+			case 3 : {
+
+				menuControl.showCoffeeInform(CoffeeList);
+				System.out.println("削除するメニューの番号を入力してください。");
+
+				int index = sc.nextInt();
+				menuControl.deleteCoffee(CoffeeList, index);
+
+				break;
+			}
+			
+			case 4 : {
+				menuControl.showCoffeeInform(CoffeeList);
+				System.out.println("修正するメニューの番号を入力してください。");
+				int index = sc.nextInt();
+				System.out.println("新しい" + CoffeeList[index] + "の金額を入力してください。");
+				int price = sc.nextInt();
+				menuControl.updateCoffeePrice(CoffeeList, index, price);
+				break;	
+			}
+			
+			case 0 : {
+				menuControl.insertInform(CoffeeList);
+				break;
+			}
+			case 9 : {
+				break;
+			}
+			}
+			
+			if( menu == 9) {
+				System.out.println("終了します！");
+				break;
+			}
+		}
 
 	}
 
--- src/MenuControl.java
+++ src/MenuControl.java
@@ -1,8 +1,5 @@
-
 public class MenuControl {
 
-	
-	
 	public boolean insertCoffee(Coffee[] CoffeeList, String name, int price) {
 
 		Coffee coffee = new Coffee();
@@ -11,28 +8,43 @@ public class MenuControl {
 
 		int index = searchIndex(CoffeeList);
 		CoffeeList[index] = coffee;
-		
+
 		System.out.println(CoffeeList[index]);
 
 		return true;
 
 	}
-	
+
 	public boolean deleteCoffee(Coffee[] CoffeeList, int index) {
-		
-		showCoffeeInform(CoffeeList);
-	
+
 		CoffeeList[index] = null;
-		
+
+		for (int i = index; i < CoffeeList.length; i++) {
+
+			if (i < CoffeeList.length - 1) {
+				CoffeeList[i] = CoffeeList[i + 1];
+			}
+
+			else {
+				CoffeeList[i] = null;
+			}
+		}
+
 		return true;
 	}
-	
+
+	public boolean updateCoffeePrice(Coffee[] CoffeeList, int index, int price) {
+
+		CoffeeList[index].price = price;
+		return true;
+	}
+
 	public boolean showCoffeeInform(Coffee[] CoffeeList) {
-		
-		for (int i=0; i<CoffeeList.length; i++) {
-			System.out.println(CoffeeList[i]);
+
+		for (int i = 0; i < CoffeeList.length; i++) {
+			System.out.println(i + ":" + CoffeeList[i]);
 		}
-		
+
 		return true;
 	}
 
@@ -46,8 +58,25 @@ public class MenuControl {
 				break;
 			}
 		}
-		
+
 		return index;
+	}
+
+	public boolean insertInform(Coffee[] CoffeeList) {
+
+		for (int i = 0; i < CoffeeList.length; i++) {
+			
+			Coffee coffee = new Coffee();
+			coffee.setName("COFFEE" + (i+1));
+			coffee.setPrice((i+1) * 100);
+			coffee.setSize("M");
+			coffee.setIce(false);
+			
+			CoffeeList[i] = coffee;
+			
+		}
+
+			return true;
 	}
 
 }
